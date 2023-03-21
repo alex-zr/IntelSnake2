8a0efca1984a37d6ec0f227c55d791403a99af80
alex.ros <alexander.ros@mintymint.net>
2023-03-21 18:05:51 +0200

    Extract nearest solver

--- src/main/java/jon/com/ua/client/YourSolver.java
+++ src/main/java/jon/com/ua/client/YourSolver.java
@@ -4,19 +4,16 @@ import com.codenjoy.dojo.client.Solver;
 import com.codenjoy.dojo.services.Dice;
 import com.codenjoy.dojo.services.Direction;
 import com.codenjoy.dojo.services.Point;
-import com.codenjoy.dojo.services.PointImpl;
 import jon.com.ua.view.View;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * User: your name
  */
 public class YourSolver implements Solver<Board> {
-    public static final int REDUCE_SIZE = 20;
+
 
     private Dice dice;
     private Board board;
@@ -32,24 +29,8 @@ public class YourSolver implements Solver<Board> {
         if (board.isGameOver()) {
             return Direction.UP.toString();
         }
-
-        Point head = board.getHead();
-        Point target = board.getSnake().size() < REDUCE_SIZE ? board.getApples().get(0) : board.getStones().get(0);
-        this.path = new ArrayList<>();
-        this.path.add(head);
-        this.path.add(target);
-        Direction direction = Stream.of(
-                        new PointImpl(head.getX() - 1, head.getY()),
-                        new PointImpl(head.getX() + 1, head.getY()),
-                        new PointImpl(head.getX(), head.getY() + 1),
-                        new PointImpl(head.getX(), head.getY() - 1)
-                )
-                .filter(p -> !board.getSnake().contains(p))
-                .filter(p -> !board.getWalls().contains(p))
-                .min(Comparator.comparingDouble(target::distance))
-                .map(head::direction)
-                .orElse(Direction.UP);
-        return direction.toString();
+        path = new ArrayList<>();
+        return NearestEmpty.getDirection(board, path);
     }
 
     public List<Point> getPath() {
